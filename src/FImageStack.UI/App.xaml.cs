20009e60d370fed35f3833f07817d065187d6b8b
Phong Võ <phong.vo@mylangroup.com>
2026-09-09 15:31:22 +0700

    feat: implement global unhandled exception logging to app_crash.log in App.xaml.cs

--- src/FImageStack.UI/App.xaml.cs
+++ src/FImageStack.UI/App.xaml.cs
@@ -1,6 +1,7 @@
-using System.Configuration;
-using System.Data;
+using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FImageStack.UI;
 
@@ -9,4 +10,21 @@ namespace FImageStack.UI;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    public App()
+    {
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    }
+
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app_crash.log");
+        File.AppendAllText(logPath, $"[{DateTime.Now}] Dispatcher Exception: {e.Exception}\n");
+    }
+
+    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app_crash.log");
+        File.AppendAllText(logPath, $"[{DateTime.Now}] Domain Exception: {e.ExceptionObject}\n");
+    }
 }
