d7367c8ab4b4767b17e7532ea73118b596ad41ff
Phong Võ <phong.vo@mylangroup.com>
2026-09-13 08:09:10 +0700

    feat(ui): integrate ZeroUI 1.5.0 obsidian theme and render optimizer

--- src/FImageStack.UI/App.xaml.cs
+++ src/FImageStack.UI/App.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Threading;
+using ZeroUI.Wpf.Theme;
 
 namespace FImageStack.UI;
 
@@ -14,6 +15,32 @@ public partial class App : System.Windows.Application
     {
         DispatcherUnhandledException += App_DispatcherUnhandledException;
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        ZeroThemeEngine.Initialize(this, "obsidian_dark");
+        ApplyThemeAliases();
+        ZeroWpfTheme.ThemeChanged += ApplyThemeAliases;
+
+        base.OnStartup(e);
+    }
+
+    private void ApplyThemeAliases()
+    {
+        Resources["BgDark"] = ZeroWpfTheme.BgPrimary;
+        Resources["BgPanel"] = ZeroWpfTheme.BgCard;
+        Resources["BgCard"] = ZeroWpfTheme.BgCard;
+        Resources["BgInput"] = ZeroWpfTheme.BgInput;
+        Resources["BorderDefault"] = ZeroWpfTheme.BorderDefault;
+        Resources["BorderHover"] = ZeroWpfTheme.BorderFocus;
+        Resources["PrimaryAccent"] = ZeroWpfTheme.PrimaryAccent;
+        Resources["SuccessAccent"] = ZeroWpfTheme.SuccessAccent;
+        Resources["WarningAccent"] = ZeroWpfTheme.WarningAccent;
+        Resources["DangerAccent"] = ZeroWpfTheme.DangerAccent;
+        Resources["TextPrimary"] = ZeroWpfTheme.TextPrimary;
+        Resources["TextSecondary"] = ZeroWpfTheme.TextSecondary;
+        Resources["TextMuted"] = ZeroWpfTheme.TextMuted;
     }
 
     private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
